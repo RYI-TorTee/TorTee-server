d5856f4a8ab11f413633a390ec5f6b5afd06f7d2
supernhilty <nhilty302814@gmail.com>
2024-06-03 14:17:34 +0700

    [Validation][Nhi] update validation for mentor application

--- TorTee.BLL/Services/MentorApplicationService.cs
+++ TorTee.BLL/Services/MentorApplicationService.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using TorTee.BLL.Exceptions;
 using TorTee.BLL.Models;
 using TorTee.BLL.Models.Requests.MentorApplications;
 using TorTee.BLL.Services.IServices;
@@ -12,16 +14,24 @@ namespace TorTee.BLL.Services
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
         private readonly IFileStorageService _fileStorageService;
+        private readonly UserManager<User> _userManager;
         public MentorApplicationService(IUnitOfWork unitOfWork,
-            IMapper mapper, IFileStorageService fileStorageService)
+            IMapper mapper, IFileStorageService fileStorageService, UserManager<User> userManager)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
             _fileStorageService = fileStorageService;
+            _userManager = userManager;
         }
         public async Task<ServiceActionResult> CreateMentorApplication(CreateMentorApplicationRequest applicationRequest)
         {
             var mentorApplication = _mapper.Map<MentorApplication>(applicationRequest);
+
+            var user = await _userManager.FindByEmailAsync(applicationRequest.Email);
+
+            if (user != null)
+                throw new BusinessRuleException($"This email has used in our system");
+
             try
             {
                 var CV = await _fileStorageService.UploadFileBlobAsync(applicationRequest.CV);
@@ -32,7 +42,7 @@ namespace TorTee.BLL.Services
             }
             catch
             {
-                throw new Exception("Can not upload your CV");
+                throw new BusinessRuleException("Can not upload your CV");
             }
             
         }
