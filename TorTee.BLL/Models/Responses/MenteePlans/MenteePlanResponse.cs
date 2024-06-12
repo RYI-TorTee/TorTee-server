d8fb5b6e730b0d502f74179bd086009a6d4cbb1d
supernhilty <nhilty302814@gmail.com>
2024-06-12 16:52:27 +0700

    [MenteePlan][Nhi] add mentee plan endpoint

--- TorTee.BLL/Models/Responses/MenteePlans/MenteePlanResponse.cs
+++ TorTee.BLL/Models/Responses/MenteePlans/MenteePlanResponse.cs
@@ -1,21 +1,12 @@
-﻿using TorTee.BLL.Models.Responses.Mentors;
-using TorTee.Core.Domains.Entities;
-using TorTee.Core.Domains.Enums;
-
-namespace TorTee.BLL.Models.Responses.MenteePlans
+﻿namespace TorTee.BLL.Models.Responses.MenteePlans
 {
     public class MenteePlanResponse
     {
         public string? DescriptionOfPlan { get; set; }
         public int CallPerMonth { get; set; }
         public int DurationOfMeeting { get; set; }
-        public int TotalSlot { get; set; }
+        public int RemainSlot { get; set; }
         public double Price { get; set; }
-
-        public Guid MentorId { get; set; }
-        public User Mentor { get; set; } = null!;
-
-        public ICollection<MenteeApplication>? MenteeApplications { get; set; }
-        //public MentorOverviewResponse Mentor { get; set; } = null!;
+        public string? Status { get; set; }
     }
 }
---
+++ TorTee.BLL/Services/MenteePlanService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TorTee.BLL.Models;
+using TorTee.BLL.Services.IServices;
+using TorTee.DAL;
+
+namespace TorTee.BLL.Services
+{
+    public class MenteePlanService : IMenteePlanService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MenteePlanService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ServiceActionResult> GetPlan(Guid mentorId)
+        {
+            var plan = (await _unitOfWork.MentorPlanRepository.GetAllAsyncAsQueryable())
+                .Include(mp=>mp.MenteeApplications)
+                .Where(mp => mp.MentorId == mentorId).FirstOrDefault() ?? throw new NullReferenceException();
+
+            return new ServiceActionResult();
+        }
+    }
+}
