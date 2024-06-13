0ca9c7db7eebf891814bab6ee88c690d0cd61c9a
supernhilty <nhilty302814@gmail.com>
2024-06-13 11:21:07 +0700

    [Workspace][Nhi] Finish workspace

--- TorTee.API/Controllers/WorkspaceController.cs
+++ TorTee.API/Controllers/WorkspaceController.cs
@@ -23,25 +23,22 @@ namespace TorTee.API.Controllers
         [HttpGet("mentee/my-assignments")]
         public async Task<IActionResult> GetAssignmentsReceived()
         {
-            Guid currentUser = new Guid("5a08a055-6168-4c7d-8158-08dc845d49d6");
             return await ExecuteServiceLogic(
-                async () => await _workspaceService.GetMenteeAssignments(currentUser).ConfigureAwait(false)
+                async () => await _workspaceService.GetMenteeAssignments(_userClaims.UserId).ConfigureAwait(false)
             ).ConfigureAwait(false);
         }
 
         [HttpGet("mentee/my-submissions")]
         public async Task<IActionResult> GetSubmissionSent()
         {
-            Guid currentUser = new Guid();
             return await ExecuteServiceLogic(
-                async () => await _workspaceService.GetSentSubmission(currentUser).ConfigureAwait(false)
+                async () => await _workspaceService.GetSentSubmission(_userClaims.UserId).ConfigureAwait(false)
             ).ConfigureAwait(false);
         }
 
         [HttpPost("mentee/submit")]
         public async Task<IActionResult> CreateSubmission([FromForm] CreateSubmissionRequest request)
-        {
-           
+        {           
             return await ExecuteServiceLogic(
                 async () => await _workspaceService.CreateASubmission(request, _userClaims.UserId).ConfigureAwait(false)
             ).ConfigureAwait(false);
@@ -49,12 +46,9 @@ namespace TorTee.API.Controllers
 
         [HttpGet("mentee/my-mentors")]
         public async Task<IActionResult> GetMyMentors()
-        {
-            Guid currentUser = new Guid("5a08a055-6168-4c7d-8158-08dc845d49d6");
-      
-           
+        {           
             return await ExecuteServiceLogic(
-                async () => await _workspaceService.GetMyMentor(currentUser).ConfigureAwait(false)
+                async () => await _workspaceService.GetMyMentor(_userClaims.UserId).ConfigureAwait(false)
             ).ConfigureAwait(false);
         }
 
@@ -76,10 +70,9 @@ namespace TorTee.API.Controllers
 
         [HttpGet("mentor/submissions-received")]
         public async Task<IActionResult> GetSubmissionsReceived()
-        {
-            Guid currentUser = new Guid();
+        {            
             return await ExecuteServiceLogic(
-                async () => await _workspaceService.GetReceivedSubmission(currentUser).ConfigureAwait(false)
+                async () => await _workspaceService.GetReceivedSubmission(_userClaims.UserId).ConfigureAwait(false)
             ).ConfigureAwait(false);
         }
 
@@ -93,17 +86,15 @@ namespace TorTee.API.Controllers
 
         [HttpGet("mentor/assignments")]
         public async Task<IActionResult> GetAssignmentsSent()
-        {
-            Guid currentUser = new Guid("34E51525-5B2C-4B66-29CD-08DC7B8919DB");
+        {            
             return await ExecuteServiceLogic(
-                async () => await _workspaceService.GetMentorAssignments(currentUser).ConfigureAwait(false)
+                async () => await _workspaceService.GetMentorAssignments(_userClaims.UserId).ConfigureAwait(false)
             ).ConfigureAwait(false);
         }
 
         [HttpPut("mentor/grade")]
         public async Task<IActionResult> GradeSubmission(GradeSubmissionRequest request)
-        {
-            Guid currentUser = new Guid();
+        {          
             return await ExecuteServiceLogic(
                 async () => await _workspaceService.UpdateGradeForSubmission(request).ConfigureAwait(false)
             ).ConfigureAwait(false);
@@ -111,10 +102,9 @@ namespace TorTee.API.Controllers
 
         [HttpGet("mentor/my-mentees")]
         public async Task<IActionResult> GetMyMentees()
-        {
-            Guid currentUser = new Guid("34E51525-5B2C-4B66-29CD-08DC7B8919DB");
+        {            
             return await ExecuteServiceLogic(
-                async () => await _workspaceService.GetMyMentee(currentUser).ConfigureAwait(false)
+                async () => await _workspaceService.GetMyMentee(_userClaims.UserId).ConfigureAwait(false)
             ).ConfigureAwait(false);
         }
     }
