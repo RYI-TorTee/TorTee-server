9923a240dc813093a87396430ceb4ac012e46b2a
supernhilty <nhilty302814@gmail.com>
2024-06-15 17:06:41 +0700

    [ChangePassword][Nhi] add change password endpoint

--- TorTee.BLL/Models/Requests/Users/UpdatePasswordRequest.cs
+++ TorTee.BLL/Models/Requests/Users/UpdatePasswordRequest.cs
@@ -1,8 +1,20 @@
-﻿namespace TorTee.BLL.Models.Requests.Users
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TorTee.BLL.Models.Requests.Users
 {
     public class UpdatePasswordRequest
     {
-        public string CurrentPassword { get; set; } = null!;
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
+        public string OldPassword { get; set; } = null!;
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
         public string NewPassword { get; set; } = null!;
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm New Password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; } = null!;
     }
 }
