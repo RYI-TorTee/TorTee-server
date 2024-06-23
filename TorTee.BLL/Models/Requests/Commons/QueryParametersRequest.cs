33c6f1f95f694f821086e9f8ec5a46f51d798790
supernhilty <nhilty302814@gmail.com>
2024-06-23 09:31:01 +0700

    [QueryParam][Nhi] update allow null empty param

--- TorTee.BLL/Models/Requests/Commons/QueryParametersRequest.cs
+++ TorTee.BLL/Models/Requests/Commons/QueryParametersRequest.cs
@@ -3,7 +3,7 @@
     public class QueryParametersRequest : PagingRequest
     {
         public IDictionary<string, object>? Filter { get; set; }
-        public string Search { get; set; } = string.Empty;
+        public string? Search { get; set; } = string.Empty;
         public string? OrderBy { get; set; }
         public bool IsDesc { get; set; } = false;
     }
